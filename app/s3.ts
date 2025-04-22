e37a8986e21981379699cd7e93b56ce2e8943cb3
itsnothuy <huytrngqu@gmail.com>
2025-04-22 02:39:10 -0400

    fix Fileupload

--- app/s3.ts
+++ app/s3.ts
@@ -51,6 +51,6 @@ export async function uploadToS3(
 }
 
 export function getS3Url(file_key: string) {
-  const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.us-east-2.amazonaws.com/${file_key}`;
+  const url = `https://${process.env.S3_BUCKET_NAME}.s3.us-east-2.amazonaws.com/${file_key}`;
   return url;
 }
