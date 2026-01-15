32039119ae3b68e4699d8fb0b5a5c3097a9dfa70
itsnothuy <huytrngu@gmail.com>
2026-01-16 04:15:19 +0700

    chore: Exclude codepair reference codebase from compilation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Exclude codepair reference folder from Next.js compilation
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/codepair/**', '**/node_modules/**'],
+    };
+    return config;
+  },
+  // Exclude from page routing
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+};
 
 export default nextConfig;
